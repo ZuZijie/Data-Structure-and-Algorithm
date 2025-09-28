931743ed65f28fe35922a0f0e8ea6cb0857fe854
ZuZijie <14937125+zoinve@user.noreply.gitee.com>
2025-09-28 23:21:43 +0800

    Week3

---
+++ Week3/src/MyQueue.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#define MAXSIZE 11
+#define true 1
+#define false 0
+typedef int DataType;
+
+typedef struct queue
+{
+	DataType* base;
+	int front;
+	int rear;
+	int maxsize;
+}Queue, *PQueue;
+
+PQueue CreatQueue(int maxsize);
+void DestroyQueue(PQueue q);
+_Bool Insert(PQueue q, DataType x);
+_Bool Delete(PQueue q, DataType* x);
+_Bool Empty(PQueue q);
+_Bool Full(PQueue q);
+int Length(PQueue q);
